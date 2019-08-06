d0711ed3a839d5ae89044a7a4213c070157b687a
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2019-08-06 08:48:16 -0300

    use iso8601 date especification as recommended

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -3,7 +3,7 @@ class ContactSerializer < ActiveModel::Serializer
 
   def attributes(*args)
     hash = super(*args)
-    hash[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
+    hash[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
     hash
   end
 end
