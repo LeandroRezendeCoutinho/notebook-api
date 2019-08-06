41f6f1766b110381a7313c1472ecd161e2fe85aa
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2019-08-06 08:00:32 -0300

    override as_json

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,4 +5,10 @@ class Contact < ApplicationRecord
 
   accepts_nested_attributes_for :phones, allow_destroy: true
   accepts_nested_attributes_for :address
+
+  def as_json(options = {})
+    hash = super(options)
+    hash[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
+    hash
+  end
 end
