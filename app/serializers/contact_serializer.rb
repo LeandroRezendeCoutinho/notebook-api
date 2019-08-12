f00e29ddba0314c2edf9f6a9bc52e10956ff0be4
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2019-08-12 08:42:06 -0300

    add HATEOS link to relationship

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,10 +1,11 @@
 class ContactSerializer < ActiveModel::Serializer
-  belongs_to :kind
+  belongs_to :kind do
+    link(:kind) { kind_url(object.kind.id) }
+  end
   has_many :phones
   has_one :address
 
   link(:self) { contact_url(object.id) }
-  link(:kind) { kind_url(object.kind.id) }
 
   attributes :id, :name, :email, :birthdate
 
