9ecccb2190e443e93070a7d104abf92bf58355ef
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2019-08-29 08:54:58 -0300

    add update on address through contact

--- app/controllers/concerns/address_controller.rb
+++ app/controllers/concerns/address_controller.rb
@@ -1,13 +1,39 @@
 class AddressesController
-  before_action :set_address
+  before_action :set_contact
 
   def show
-    render json: @addresses
+    render json: @addresses.address
+  end
+
+  def update
+    if @contact.address.update(address_params)
+      render json: @contact.address
+    else
+      render json: @contact.errors, status: :unprocessable_entity
+    end
+  end
+
+  def create
+    @contact.address = Address.new(address_params)
+
+    if @contact.save
+      render json: @contact.address, status: :created
+    else
+      render json: @contact.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @contact.address.destroy
   end
 
   private
 
   def set_address
-    @address = Contact.find(params[:contact_id]).address
+    @contact = Contact.find(params[:contact_id])
+  end
+
+  def address_params
+    ActiveModelSerializer::Deserialization.jsonapi_parse(params)
   end
 end
