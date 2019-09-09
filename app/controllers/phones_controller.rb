55a827f667234210be034d7dcb070adc3993136d
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2019-09-09 08:18:01 -0300

    add phones through contact

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -1,18 +1,41 @@
 class PhonesController < ApplicationController
-  before_action :set_phones
+  before_action :set_contact
 
   def show
-    render json: @kind
+    render json: @contact.phones
+  end
+
+  def create
+    @contact.phones << Phone.new(phone_params)
+
+    if @contact.save
+      render json: @contact.phones, status: :created, location: contact_phones_url(@contact)
+    else
+      render json: @contact.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    phone = Phone.find(phone_params[:id])
+    if phone.update(phone_params)
+      render json: @contact.phones
+    else
+      render json: @contact.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    phone = Phone.find(phone_params[:id])
+    phone.destroy
   end
 
   private
 
-  def set_phones
-    if params[:contact_id]
-      @phones = Contact.find(params[:contact_id]).phones
-      return @phones
-    end
+  def set_contact
+    @contact = Contact.find(params[:contact_id])
+  end
 
-    @phones = phones.where(id: params[:id])
+  def phone_params
+    ActiveModelSerializers::Deserialization.jsonapi_parse(params)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
   resources :contacts do
     resource :kind, only: [:show]
     resource :kind, only: [:show], path: 'relationships/kind'
+
     resource :phones, only: [:show]
     resource :phones, only: [:show], path: 'relationships/phones'
+
+    resource :phone, only: [:update, :destroy, :create]
+
     resource :address, only: [:show, :update, :destroy, :create]
     resource :address, only: [:show, :update, :destroy, :create], path: 'relationships/address'
   end
