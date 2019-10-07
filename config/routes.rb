16255a85750bf6e020f4013cac886ec2d30fcf6b
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2019-10-07 08:39:55 -0300

    versioning controller with query param

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,32 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auths'
   resources :auths, only: [:create]
   resources :kinds
-  resources :contacts do
-    resource :kind, only: [:show]
-    resource :kind, only: [:show], path: 'relationships/kind'
+  scope module: 'v1' do
+    resources :contacts, constraints: lambda { |request| request.params[:version] == '1' }  do
+      resource :kind, only: [:show]
+      resource :kind, only: [:show], path: 'relationships/kind'
 
-    resource :phones, only: [:show]
-    resource :phones, only: [:show], path: 'relationships/phones'
+      resource :phones, only: [:show]
+      resource :phones, only: [:show], path: 'relationships/phones'
 
-    resource :phone, only: [:update, :destroy, :create]
+      resource :phone, only: [:update, :destroy, :create]
 
-    resource :address, only: [:show, :update, :destroy, :create]
-    resource :address, only: [:show, :update, :destroy, :create], path: 'relationships/address'
+      resource :address, only: [:show, :update, :destroy, :create]
+      resource :address, only: [:show, :update, :destroy, :create], path: 'relationships/address'
+    end
+  end
+  scope module: 'v2' do
+    resources :contacts, constraints: lambda { |request| request.params[:version] == '2' } do
+      resource :kind, only: [:show]
+      resource :kind, only: [:show], path: 'relationships/kind'
+
+      resource :phones, only: [:show]
+      resource :phones, only: [:show], path: 'relationships/phones'
+
+      resource :phone, only: [:update, :destroy, :create]
+
+      resource :address, only: [:show, :update, :destroy, :create]
+      resource :address, only: [:show, :update, :destroy, :create], path: 'relationships/address'
+    end
   end
 end
