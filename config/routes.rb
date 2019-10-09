0d1eb99792ac9af01c92ec684d7925239b5b097e
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2019-10-09 08:00:36 -0300

    change to http header versioning

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auths'
   resources :auths, only: [:create]
   resources :kinds
-  api_version(:module => "V1", :parameter => {:name => "version", :value => "1"}) do
+  api_version(:module => "V1", :header => {:name => "Accept", :value => "Application/json; version=1"}) do
     resources :contacts do
       resource :kind, only: [:show]
       resource :kind, only: [:show], path: 'relationships/kind'
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
       resource :address, only: [:show, :update, :destroy, :create], path: 'relationships/address'
     end
   end
-  api_version(:module => "V2", :parameter => {:name => "version", :value => "2"}) do
+  api_version(:module => "V2",  :header => {:name => "Accept", :value => "Application/json; version=2"}) do
     resources :contacts do
       resource :kind, only: [:show]
       resource :kind, only: [:show], path: 'relationships/kind'
