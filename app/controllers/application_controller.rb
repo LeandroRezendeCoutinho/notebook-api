5e8eaf32ead696583c58a9d86601d9d56b50272d
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2019-10-14 09:17:28 -0300

    add Content-Type to force json on content

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
-  # before_action :ensure_json_request
+  before_action :ensure_json_request
 
-  # def ensure_json_request
-  #   return if request.headers['Accept'] =~ /vnd\.api\+json/
+  def ensure_json_request
+    render nothing: true, status: 406 unless request.headers['Accept'] =~ /json/
 
-  #   render nothing: true, status: 406
-  # end
+    return if request.get?
+
+    return if request.headers['Content-Type'] =~ /json/
+
+    render nothing: true, status: 415
+  end
 end
