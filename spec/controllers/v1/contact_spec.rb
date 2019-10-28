ee8a62edff390e1ee574406e26b527ed287a1630
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2019-10-28 08:14:43 -0300

    make tests in a better way

--- spec/controllers/v1/contact_spec.rb
+++ spec/controllers/v1/contact_spec.rb
@@ -3,22 +3,15 @@
 require 'rails_helper'
 
 describe V1::ContactsController, type: :controller do
-  # just for example. Dont use this way
   it 'request index and return 200 OK' do
     request.accept = 'application/json'
     get :index
-    expect(response.status).to eql(200)
-  end
-
-  it 'request index and return 200 OK' do
-    request.accept = 'application/json'
-    get :index
-    expect(response).to have_http_status(200)
+    expect(response).to have_http_status(:ok)
   end
 
   it 'request index and return 406 NOT ACCEPTABLE' do
     get :index
-    expect(response).to have_http_status(406)
+    expect(response).to have_http_status(:not_acceptable)
   end
 
 end
